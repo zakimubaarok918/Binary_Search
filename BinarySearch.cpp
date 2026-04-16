8980ce2129ceeb2b6d2430194245a6091aa0a992
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-16 15:53:28 +0700

    Implement Bubble Sort algorithm

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -21,3 +21,21 @@ void input() {
         cin >> element[i];
     }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang - 1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
+        }
+    } while (pass++ < nPanjang - 1);
+}
+
