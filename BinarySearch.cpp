ce9bf81ed147c484325fa0be5f210874b340a312
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-16 15:54:47 +0700

    Add display function for array output

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -39,3 +39,20 @@ void bubbleSortArray()
     } while (pass++ < nPanjang - 1);
 }
 
+void display()
+{
+    cout << "\n====================================\n";
+    cout << " Elemen array setelah diurutkan: \n";
+    cout << "\n====================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j] << " ";
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
