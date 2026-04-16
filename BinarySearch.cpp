ea58a02c83ab68d4e09334f52cd20c288579481b
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-04-16 16:01:28 +0700

    Fix syntax error: Incorrect function parameter in input()

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -103,3 +103,31 @@ void binarySearch()
 
     }while (ulang == 'y' || ulang == 'y');
 }
+
+void input() 
+{
+    while (true) 
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10) 
+        {
+            break;    
+        }
+        else 
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. silakan coba lagi.\n";
+        }
+    }
+
+    cout << "\n====================================\n";
+    cout << "         Masukkan elemen array         \n";
+    cout << "\n====================================\n";
+
+    for (int i = 0; i < nPanjang; i++) 
+    {
+        cout << "Data ke-" << i + 1 << ": ";
+        cin >> element[i];
+    }
+}
